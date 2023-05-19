79316f76b59f8a19c0ae411e3e00662db252de62
nahian255 <nahimn253@gmail.com>
2023-05-19 23:54:54 +0600

    toy gallery done

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -3,14 +3,13 @@ import logo from "../../../assets/bannerImg.jpg"
 
 const Banner = () => {
     return (
-        <div className="hero min-h-screen my-8" style={{ backgroundImage: `url(${logo})` }}>
+        <div className="hero min-h-screen lg:h-52 my-8" style={{ backgroundImage: `url(${logo})` }}>
             {/* <img src="https://postimg.cc/NL40J1jr" alt="" /> */}
             <div className="hero-overlay bg-opacity-60"></div>
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-md">
-                    <h1 className="mb-5 text-5xl font-bold">Action Herors Toy's Hub</h1>
-                    <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
-                    <button className="btn btn-primary">Get Started</button>
+                    <h1 className="mb-5 text-5xl text-green-500 font-bold">Action Herors Toys MarketPlace</h1>
+                    <p className="mb-5 my-8 text-teal-300">A toy is an object that's made for a child to play with. Your favorite Action heroes toy might have been a hulk, Captian america or it would be the DC. Some toys are specifically designed for play, like a small wooden train set or a baby's shape sorter.</p>
                 </div>
             </div>
         </div>
--- src/Pages/Home/Gallery/Gallery.jsx
+++ src/Pages/Home/Gallery/Gallery.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
+import img1 from "../../../assets/img1.jpg"
+import img2 from "../../../assets/img8.jpg"
+import img3 from "../../../assets/img7.jpg"
+import img4 from "../../../assets/img4.jpg"
+import img5 from "../../../assets/img5.jpg"
 
 const Gallery = () => {
     return (
-        <div>
-            <h1 className='text-4xl text-green-600'>Action figure toys Gallery</h1>
-
+        <div className=''>
+            <h1 className='text-4xl my-8 text-green-600'>Action figure toys Gallery</h1>
+            <p className='my-4 text-black text-xl'>Pick Your favourait one</p>
+            <div className="carousel carousel-end rounded-box">
+                <div className="carousel-item">
+                    <img src={img1} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img2} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img3} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img1} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img2} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img1} alt="Drink" />
+                </div>
+                <div className="carousel-item">
+                    <img src={img1} alt="Drink" />
+                </div>
+            </div>
         </div>
     );
 };
